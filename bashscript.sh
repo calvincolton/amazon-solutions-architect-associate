5ada025594e7a613de7937b58f65d34668d37517
Calvin Colton <calvincolton@gmail.com>
2018-11-27 21:08:16 -0800

    Lambda and Polly Study Alexa App

--- bashscript.sh
+++ bashscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum update -y
 yum install httpd -y
+aws s3 cp s3://test-us-east-1-virginia-bucket /var/www/html --recursive
 service httpd start
 chkconfig httpd on
-aws s3 cp s3://test-us-east-1-virginia-bucket/index.html /var/www/html
